9d7425901a68f7ada865f36ed33baafb9a61ff4f
tejaswi0905 <73959528+tejaswi0905@users.noreply.github.com>
2024-05-25 01:27:12 +0530

    Done with callbacks, OAuth with google and github, todo sending emails

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import * as z from "zod";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
@@ -15,7 +15,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   }
 
   const { email, password, name } = validatedFields.data;
-  const hashedPassword = await bcrypt.hash(password, 10);     
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const existingUser = await getUserByEmail(email);
 
--- auth.ts
+++ auth.ts
@@ -3,8 +3,32 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { db } from "./lib/db";
 import authConfig from "./auth.config";
+import { getUserById } from "./data/user";
+import { UserRole } from "@prisma/client";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) {
+        return token;
+      }
+      const user = await getUserById(token.sub);
+      if (!user) {
+        return token;
+      }
+      token.role = user.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
